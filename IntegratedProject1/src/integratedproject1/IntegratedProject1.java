f3da9f257f76e0bfda07f7dbd566a55f08690669
arazaq203 <14razaq@gamil.com>
2019-02-12 10:59:57 +0000

    Changed back to Hello World

--- IntegratedProject1/src/integratedproject1/IntegratedProject1.java
+++ IntegratedProject1/src/integratedproject1/IntegratedProject1.java
@@ -22,7 +22,7 @@ public class IntegratedProject1 extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello Adam'");
+        btn.setText("Say 'Hello world'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
