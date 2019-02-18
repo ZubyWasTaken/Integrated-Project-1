43cb2af5019172c70432fee255812d51a64267a5
Zubair <zubairkhalid12@gmail.com>
2019-02-18 21:02:36 +0000

    Added comments, and created json files to debug, updated gitignore

--- IntegratedProject1/src/integratedproject1/IntegratedProject1.java
+++ IntegratedProject1/src/integratedproject1/IntegratedProject1.java
@@ -21,23 +21,37 @@ public class IntegratedProject1 extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        // Creates new button object called btn
         Button btn = new Button();
+
+        //Sets the visable text on the button object to display "Say 'Hello World'"'
         btn.setText("Say 'Hello World'");
+
+        //When the button object recieves an action, a new event handler is created to handle that action
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
+            //This handles that event
             public void handle(ActionEvent event) {
+                //Outputs to console "Hello World!"
                 System.out.println("Hello World!");
             }
         });
         
+        //New Stackpane created called 'root'.
+        // This layers its children back to front
         StackPane root = new StackPane();
+        //The root object gets it's children as a list and adds button as another child
         root.getChildren().add(btn);
         
+        //A new scene called scene is created, with the root as the main scene, and the height and width of the scene set.
         Scene scene = new Scene(root, 300, 250);
         
+        //Sets title to "Hello World!"
         primaryStage.setTitle("Hello World!");
+        //Specify's what scene to be used here
         primaryStage.setScene(scene);
+        //Displays that scene
         primaryStage.show();
     }
 
