67deb7530b486e689bb56bb640a83075da143d81
Zubair <zubairkhalid12@gmail.com>
2019-02-26 21:11:25 +0000

    Messed up last merge lol

---
+++ IntegratedProject1/src/UserLogin.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class UserLogin {
+
+    public static void display(String title, String message) {
+
+    }
+    
+}
