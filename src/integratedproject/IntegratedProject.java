57953e7590d4b07a626746d64a7e07325f46d550
Zubair <zubairkhalid12@gmail.com>
2019-03-05 20:02:27 +0000

    .

--- src/integratedproject/IntegratedProject.java
+++ src/integratedproject/IntegratedProject.java
@@ -19,7 +19,7 @@ public class IntegratedProject extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXML/MainMenu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
         
         Scene scene = new Scene(root);
         
