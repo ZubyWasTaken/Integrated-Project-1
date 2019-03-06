2f8a12f86bf9700900d83e537d078da09390632b
Zubair <zubairkhalid12@gmail.com>
2019-03-06 21:30:04 +0000

    Validated inputs
    Input validated all the data, and displayed according prompt errors,
    and once all the data has been entered properly it writes that data
    to the text file.

--- src/integratedproject/FXMLDocumentController.java
+++ src/integratedproject/FXMLDocumentController.java
@@ -34,6 +34,10 @@ import javafx.stage.StageStyle;
  */
 public class FXMLDocumentController implements Initializable {
 
+    /*
+     This methoid below writes all the user data to a text file, with the userID
+     as the file name.
+     */
     public void writeToFile(String forename, String surname, String userID, String password, LocalDate dateOfBirth) throws IOException {
 
         try (PrintWriter writer = new PrintWriter("UserData/" + userID + ".txt", "UTF-8")) {
@@ -47,6 +51,10 @@ public class FXMLDocumentController implements Initializable {
 
     }
 
+    /*
+     These variables below link the FXML labels and text fields
+     with the code, allowing the code to manipulate them.
+     */
     @FXML
     private TextField forename1;
 
@@ -63,36 +71,42 @@ public class FXMLDocumentController implements Initializable {
     private DatePicker DoB;
 
     @FXML
+    private Label forenameLabel;
+
+    @FXML
+    private Label surnameLabel;
+
+    @FXML
+    private Label passwordLabel;
+
+    @FXML
+    private Label confirmPasswordLabel;
+
+    @FXML
+    private Label dateLabel;
+
+    @FXML
+
+    /*
+     This code is executed when the 'Register' button is clicked in
+     Register.fxml
+     */
     private void registerButtonAction(ActionEvent event) throws IOException {
+
+        /*
+         Setting the data in the textfields and date picker to variables
+         to be used by the code.
+         */
         String forename = forename1.getText();
         String surname = surname2.getText();
         String passwrd = password.getText();
         String confirmPasswrd = confirmPassword.getText();
         LocalDate dateOfBirth = DoB.getValue();
 
-        boolean match = passwrd.equals(confirmPasswrd);
-        if (match == true) {
-            System.out.println("Your passwords match.");
-        } else {
-            System.out.println("Your passwords do not match.");;
-        }
-        if (passwrd.length() == 0) {
-            System.out.println("Fill in password");
-        }
-        if (forename.length() == 0) {
-            System.out.println("Fill in forename");
-        }
-        if (surname.length() == 0) {
-            System.out.println("Fill in surname");
-        }
-        if (dateOfBirth == null) {
-            System.out.println("No valid date");
-        }
-        if (passwrd.length() < 8 || passwrd.length() > 18) {
-            System.out.println("Password must be 8 to 18 characters in length.");
-
-        }
-
+        /*
+         This is to affix a random number to the user's forename and surname
+         to create a unique userID.
+         */
         int max = 1000;
         int min = 1;
         Random rand = new Random();
@@ -100,8 +114,101 @@ public class FXMLDocumentController implements Initializable {
 
         String userID = forename + surname + randomNum;
 
-        writeToFile(forename, surname, userID, passwrd, dateOfBirth);
+        /*
+         Checks if passwords are equal.
+         */
+        boolean match = passwrd.equals(confirmPasswrd);
+        if (match == true) {
+            System.out.println("Your passwords match.");
 
+        } else {
+            System.out.println("Your passwords do not match.");;
+            confirmPasswordLabel.setText("Your passwords do not match.");
+        }
+
+        /*
+         This section checks if the textfields and datepicker are empty
+         and if they are empty to change the text in the labels to prompt the
+         user to enter the data.
+         */
+        if (forename.length() == 0) {
+            System.out.println("Fill in forename");
+            forenameLabel.setText("Please enter your forename.");
+        }
+        if (surname.length() == 0) {
+            System.out.println("Fill in surname");
+            surnameLabel.setText("Please enter your surname.");
+        }
+        if (passwrd.length() == 0) {
+            System.out.println("Fill in password");
+            passwordLabel.setText("Please enter your password.");
+        }
+        if (confirmPasswrd.length() == 0) {
+            System.out.println("Fill in password");
+            confirmPasswordLabel.setText("Please confirm your password.");
+        }
+        if (dateOfBirth == null) {
+            System.out.println("No valid date");
+            dateLabel.setText("Please confirm your Date of Birth.");
+        }
+
+        /*
+         If the password entered is not between 8 and 18 chars it prompts
+         the user that their password must be between those two limits.
+         */
+        if ((passwrd.length() < 8 || passwrd.length() > 18)) {
+
+            passwordLabel.setText("Password must be 8 to 18 characters.");
+        }
+        
+        /*
+         If the forename and username fields and datepicker are not empty then it clears the 
+         labels.
+         */
+        if (forename.length() != 0) {
+            forenameLabel.setText("");
+        }
+        if (surname.length() != 0) {
+            surnameLabel.setText("");
+        }
+        if (dateOfBirth != null) {
+            dateLabel.setText("");
+        }
+        
+        /*
+         If the password is between the limit then it clears the label telling
+         you that your password is not within limit.
+         */
+        if ((passwrd.length() > 7 && passwrd.length() < 19)) {
+            passwordLabel.setText("");
+        }
+        
+        /*
+         Checks if your password matches, and if the field has any data in it,
+         if so then it clears the label that tells you to confirm your password.
+         */
+        if (confirmPasswrd.length() != 0 && (match == true)) {
+            confirmPasswordLabel.setText("");
+        }
+        
+        /*
+        If the forename, surname, datepicker, and both password fields are NOT
+        empty, AND if the password lengths are within limit, THEN it clears the
+        remaining labels, and calls passes the variables into the method to
+        write the data to an individual text file.
+        */
+        if (forename.length() != 0 && surname.length() != 0 && dateOfBirth != null && passwrd.length() != 0 && confirmPasswrd.length() != 0) {
+            if ((passwrd.length() > 7 && passwrd.length() < 19)) {
+                forenameLabel.setText("");
+                surnameLabel.setText("");
+                dateLabel.setText("");
+
+                writeToFile(forename, surname, userID, passwrd, dateOfBirth);
+                
+                // Closes that window
+                ((Stage) (((Button) event.getSource()).getScene().getWindow())).close();
+            }
+        }
     }
 
 
@@ -120,6 +227,11 @@ public class FXMLDocumentController implements Initializable {
         reg.show();
     }
 
+    
+    /*
+    When the Home button in Register.fxml is clicked, it closes the window,
+    effectively taking you back to Home.fxml
+    */
     @FXML
     public void homeButtonAction(ActionEvent event) {
         ((Stage) (((Button) event.getSource()).getScene().getWindow())).close();
