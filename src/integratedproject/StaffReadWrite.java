3e2683f10dcfc7c92a73550ed0042c6e2c3e7295
Zubair Khalid <zubairkhalid12@gmail.com>
2019-03-28 10:32:45 +0000

    added ReadWrite class that staff will use to login and read appointments etc

---
+++ src/integratedproject/StaffReadWrite.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package integratedproject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Zuby
+ */
+public class StaffReadWrite {
+
+    
+
+}
